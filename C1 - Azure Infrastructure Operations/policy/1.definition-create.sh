56971534a4433909ccd3acec8fbd6de37dfc7be6
BachVu89 <hoangbach89gl@gmail.com>
2024-05-23 16:38:09 +0700

    Update 1.definition-create.sh

--- C1 - Azure Infrastructure Operations/policy/1.definition-create.sh
+++ C1 - Azure Infrastructure Operations/policy/1.definition-create.sh
@@ -1,3 +1,1 @@
-az policy definition create --name tagging-policy --subscription "required tags" \
---params "{ \"tagName\": { \"type\": \"String\", \"metadata\": { \"displayName\": \"Tag Name\", \"description\": \"Name of the tag is required, such as 'environment'\" } } }" \
---rules "{\"if\": { \"field\": \"tags\", \"exists\": \"false\" }, \"then\": { \"effect\": \"deny\" }}"
+ az policy definition create --name tagging-policy --subscription "UdacityDS - 58" --rules tagging-rules.json
