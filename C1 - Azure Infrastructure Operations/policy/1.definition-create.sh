09a982abd9d9812a8b02b316395a05cf55105d0f
BachVu89 <hoangbach89gl@gmail.com>
2024-09-21 21:26:48 +0700

    Delete C1 - Azure Infrastructure Operations/policy directory

--- C1 - Azure Infrastructure Operations/policy/1.definition-create.sh
+++
@@ -1,1 +0,0 @@
- az policy definition create --name tagging-policy --subscription "UdacityDS - 58" --rules tagging-rules.json
--- C1 - Azure Infrastructure Operations/policy/2.assign-create-policy.sh
+++
@@ -1,1 +0,0 @@
-az policy assignment create --name "tagging-policy-assignment" --policy "tagging-policy" --description "required tags" --display-name "tagging-policy"
